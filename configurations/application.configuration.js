15b85b034999ffdbd96bfe42d1eb113c6d48e07b
Gabriel D'Intino <GDintino@proveedor.teco.com.ar>
2019-01-23 10:49:11 -0300

    Fixing issue.

--- configurations/application.configuration.js
+++ configurations/application.configuration.js
@@ -4,5 +4,5 @@ module.exports = {
     getApiVersion1: () => "V1",
     getApplicationUrl: () => process.env.NODEJS_SDL_API_SERVER_URL || "127.0.0.1",
     getApplicationPort: () => process.env.NODEJS_SDL_API_SERVER_PORT || 3000,
-    getApiCacheEndpoint: () => process.env.NODEJS_API_CACHE_ENDPOINT || "http://localhost:8080/cache/V1/item"
+    getApiCacheEndpoint: () => process.env.NODEJS_API_CACHE_ENDPOINT || "http://localhost:8080/cache/V1/items"
 }
